49db7fec68fdb82e34e3f9f3d42a8f1ad75c891a
vulcannos <83702767+vulcannos@users.noreply.github.com>
2024-12-07 17:48:14 +0100

    Update Testjava.java
    c'est vrais

--- Testjava.java
+++ Testjava.java
@@ -3,6 +3,6 @@ public class Testjava {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("hello world");
-		fgfdhsfqeghdgrgdyjkgjyjftykg
+		System.out.println("j'arive pas à me Conecter avec éclipse");
   	}
 }
